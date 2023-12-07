df10b87e845f7fea2891b93e214758d36b673782
Aryatch <153222778+Aryatch@users.noreply.github.com>
2023-12-07 14:09:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openSidebar(){
+    document getElementById("sidebar"),style.width="100px";
+    document getElementById(""),style.width="100px";
+}
+function closeSidebar(){
+    document getElementById("sidebar"),style.width="100px";
+    document getElementById(""),style.width="100px";
+}
